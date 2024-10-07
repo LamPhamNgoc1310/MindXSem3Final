fed2e1e6e15bc164cc7b1889dc81a5933f491bde
LamPhamNgoc1310 <phamngoclam1310@gmail.com>
2024-10-08 04:33:22 +0700

    feat: add event on card click

--- finaltest/src/layout/Home.jsx
+++ finaltest/src/layout/Home.jsx
@@ -1,36 +1,43 @@
 import "./Home.css";
-import axios from 'axios'
+import axios from "axios";
 import { IoReorderThree } from "react-icons/io5";
 import { IoSearchSharp } from "react-icons/io5";
-import { Carousel } from "antd";
+import { Carousel, Modal } from "antd";
 import { useEffect, useState } from "react";
 
 function Home() {
+  const [isDisplay, setIsDisplay] = useState(false);
+  const [selectedCard, setSelectedCard] = useState(null);
+  const showBox = (movie) => {
+    setSelectedCard(movie);
+    setIsDisplay(true);
+  };
+  const handleCancel = () => {
+    setIsDisplay(!isDisplay);
+  };
 
-
-    const [movieData, setMovieData] = useState([])
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:8080/movies')
-        setMovieData(response.data) // set movie data array
-      } catch (error) {
-        console.error('Error: ', error)
-      }
+  const [movieData, setMovieData] = useState([]);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/movies");
+      setMovieData(response.data); // set movie data array
+    } catch (error) {
+      console.error("Error: ", error);
     }
+  };
 
-    // call api only once when component mounts
-    useEffect(() => {
-        fetchData(); 
-        console.log(movieData);
-    },[]);
+  // call api only once when component mounts
+  useEffect(() => {
+    fetchData();
+    console.log(movieData);
+  }, []);
 
   const contentStyle = {
     display: "flex",
-    justifyContent: 'center',
-    alignItem: 'center',
+    justifyContent: "center",
+    alignItem: "center",
     flexDirection: "column",
     height: "350px",
-    
   };
 
   const imageStyle = {
@@ -38,6 +45,28 @@ function Home() {
   };
   return (
     <div className="container">
+      <Modal
+        title={selectedCard ? selectedCard.name : ""}
+        visible={isDisplay}
+        onCancel={handleCancel}
+      >
+        {selectedCard && (
+          <div>
+            <img
+              src={selectedCard.image}
+              alt={selectedCard.name}
+              style={{ width: "100%" }}
+            />
+            <p>{selectedCard.introduce}</p>{" "}
+            <p>
+              <strong>Time:</strong> {selectedCard.time} min
+            </p>
+            <p>
+              <strong>Year:</strong> {selectedCard.year}
+            </p>
+          </div>
+        )}
+      </Modal>
       <div className="movie-section-container">
         <br />
         <div className="header-section">
@@ -61,10 +90,15 @@ function Home() {
               draggable={true}
               slidesToShow={4}
               slidesToScroll={1}
-            > 
+            >
               {/* mapping movie data */}
               {movieData.map((movie, index) => (
-                <div className="option" key={index} style={contentStyle}>
+                <div
+                  className="option"
+                  key={index}
+                  style={contentStyle}
+                  onClick={() => showBox(movie)}
+                >
                   <div className="movie-image">
                     <img style={imageStyle} src={movie.image} />
                   </div>
